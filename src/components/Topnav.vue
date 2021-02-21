1d4625fd439a698680c1416a2ae618b0134aeb23
Joish <1270685166@qq.com>
2021-02-21 20:46:26 +0800

    简单完善css

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -9,15 +9,25 @@
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
-import {Component, Prop} from 'vue-property-decorator';
-
-@Component
-export default class Topnav extends Vue {
-
-}
+export default {};
 </script>
 
 <style lang="scss" scoped>
-
+.topnav {
+  background: pink;
+  display: flex;
+  padding: 16px;
+  > .logo {
+    max-width: 6em;
+    margin-right: auto;
+  }
+  > .menu {
+    display: flex;
+    white-space: nowrap;
+    flex-wrap: nowrap;
+    > li {
+      margin: 0 1em;
+    }
+  }
+}
 </style>
