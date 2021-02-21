50330dcb2da406b12340c63fa21bc62499a4865c
Joish <1270685166@qq.com>
2021-02-21 20:52:21 +0800

    调节Topnav和aside的位置

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -33,39 +33,6 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
-.layout {
-  display: flex;
-  flex-direction: column;
-  height: 100vh;
-
-  > .nav {
-    flex-shrink: 0;
-  }
-
-  > .content {
-    flex-grow: 1;
-    padding-top: 60px;
-    padding-left: 156px;
-    @media (max-width: 500px) {
-      padding-left: 0;
-    }
-  }
-}
-
-.content {
-  display: flex;
-
-  > aside {
-    flex-shrink: 0;
-  }
-
-  > main {
-    flex-grow: 1;
-    padding: 16px;
-    background: lightgreen;
-  }
-}
-
 aside {
   background: lightblue;
   width: 150px;
@@ -74,7 +41,6 @@ aside {
   top: 0;
   left: 0;
   padding-top: 70px;
-  height: 100%;
 
   > h2 {
     margin-bottom: 4px;
@@ -85,9 +51,5 @@ aside {
       padding: 4px 0;
     }
   }
-}
-
-main {
-  overflow: auto;
 }
 </style>
