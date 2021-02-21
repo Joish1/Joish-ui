d9916875e3b07319a2eb221a2433e1fdb043c3ba
Joish <1270685166@qq.com>
2021-02-21 18:57:31 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>hi</div>
+  <Joish/>
+</template>
+
+<script>
+import Joish from './components/Joish.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Joish
+  }
+}
+</script>
