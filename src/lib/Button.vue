a3846e088ee1610e17325ffb682318b463db80f9
Joish <1270685166@qq.com>
2021-02-22 22:49:06 +0800

    loading样式完善

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,5 +1,6 @@
 <template>
   <button class="joish-button" :class="classes" :disabled="disabled">
+    <span v-if="loading" class="joish-loadingIndicator"></span>
     <slot/>
   </button>
 </template>
@@ -24,6 +25,10 @@ export default {
     disabled: {
       type: Boolean,
       disabled: false
+    },
+    loading: {
+      type: Boolean,
+      default: false
     }
   },
   setup(props) {
@@ -188,6 +193,27 @@ $grey: grey;
       cursor: not-allowed;
       color: $grey;
     }
+  }
+
+  > .joish-loadingIndicator {
+    width: 14px;
+    height: 14px;
+    display: inline-block;
+    margin-right: 4px;
+    border-radius: 8px;
+    border-color: $blue $blue $blue transparent;
+    border-style: solid;
+    border-width: 2px;
+    animation: joish-spin 1s infinite linear;
+  }
+}
+
+@keyframes joish-spin {
+  0% {
+    transform: rotate(0deg)
+  }
+  100% {
+    transform: rotate(360deg)
   }
 }
 </style>
