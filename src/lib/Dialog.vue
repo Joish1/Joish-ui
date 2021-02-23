3b6a5557f6631da552c428299b343c0e22f7534e
Joish <1270685166@qq.com>
2021-02-23 13:13:21 +0800

    Dialog支持visble

--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -1,24 +1,32 @@
 <template>
-  <div class="joish-dialog-overlay"></div>
-  <div class="joish-dialog-wrapper">
-    <div class="joish-dialog">
-      <header>标题 <span class="joish-dialog-close"></span></header>
-      <main>
-        <p>第一行字</p>
-        <p>第二行字</p>
-      </main>
-      <footer>
-        <Button level="main">OK</Button>
-        <Button>Cancel</Button>
-      </footer>
+  <template v-if="visible">
+    <div class="joish-dialog-overlay"></div>
+    <div class="joish-dialog-wrapper">
+      <div class="joish-dialog">
+        <header>标题 <span class="joish-dialog-close"></span></header>
+        <main>
+          <p>第一行字</p>
+          <p>第二行字</p>
+        </main>
+        <footer>
+          <Button level="main">OK</Button>
+          <Button>Cancel</Button>
+        </footer>
+      </div>
     </div>
-  </div>
+  </template>
 </template>
 
 <script lang="ts">
 import Button from './Button.vue';
 
 export default {
+  props: {
+    visible: {
+      type: Boolean,
+      default: false
+    }
+  },
   components: {
     Button
   }
