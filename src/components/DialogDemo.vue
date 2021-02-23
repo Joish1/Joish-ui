7d07bc52b5b243ac54bcc4b8b4b3a7c87fb08c36
Joish <1270685166@qq.com>
2021-02-23 12:58:27 +0800

    创建Dialog组件

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,11 +1,17 @@
 <template>
-  <div>switch</div>
+  <div>Dialog</div>
+  <h1>示例1</h1>
+  <Dialog></Dialog>
 </template>
 
 <script lang="ts">
-export default {
+import Dialog from '../lib/Dialog.vue';
 
-}
+export default {
+  components: {
+    Dialog
+  }
+};
 </script>
 
 <style lang="scss" scoped>
